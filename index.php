c415c1c771b9132cc82d6f9baafe174fffd62616
Cheizerman <cheizer@list.ru>
2021-01-23 15:12:46 +0300

    UPP

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class Instagram{
     public $count_post = 0;
     public $error = "";
     public $App = "";
-    public function __construct($token, $count = 50){
+    public function __construct($token, $count = 3){
         global $APPLICATION;
         $this->token_params = $token;
         $this->count_post = $count;
@@ -70,31 +70,100 @@ class Instagram{
 ?>
 
 <?
-$token ='YOUR-TOKEN-APP-INSTAGRAM-BASIC-DISPLAY';
+$token ='YOUR-TOKEN-INSTAGRAMM';
 $inst = new Instagram($token);
-$instPosts = $inst->getInstagramPosts();
+$usersMedia = $inst->getInstagramPosts();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Instagram Feed</title>
+  <title>Document</title>
 </head>
 <body>
+<style>
+			body {
+				font-family: 'Helvetica';
+			}
+
+			.pages-list {
+				display: block;
+			}
+
+			.pages-list-item {
+				display: inline-block;
+				vertical-align: top;
+				margin-top: 10px;
+				width: 250px;
+				border: 1px solid #333;
+				margin-left: 10px;
+				padding: 10px;
+			}
+
+			.pages-media {
+				width: 100%;
+				max-width:100px;
+			}
+
+			.raw-response {
+				width: 100%;
+				height: 600px;
+			}
+
+			.nav-container {
+				margin-top: 20px;
+				font-size: 20px;
+				display: inline-block;
+				width: 100%;
+			}
+
+			.nav-next {
+				float: right;
+			}
+		</style>
+
+		<div class="nav-container">
+			<?php if ( isset( $usersMedia['paging']['previous'] ) ) : ?>
+				<a href="<?php $_SERVER['PHP_SELF']; ?>?cursor_type=before&cursor=<?php echo $usersMedia['paging']['cursors']['before']; ?>&limit=3">
+					< PREVIOUS
+				</a>
+			<?php endif; ?>
+			<?php if ( isset( $usersMedia['paging']['next'] ) ) : ?>
+				<a class="nav-next" href="<?php $_SERVER['PHP_SELF']; ?>?cursor_type=after&cursor=<?php echo $usersMedia['paging']['cursors']['after']; ?>&limit=3">
+					NEXT >
+				</a>
+			<?php endif; ?>
+		</div>
+		<ul class="pages-list">
+			<?php foreach ( $usersMedia['data'] as $media ) : // loop over posts returned for the page ?>
+				<li class="pages-list-item">
+					<?php if ( 'IMAGE' == $media['media_type'] || 'CAROUSEL_ALBUM' == $media['media_type'] ) : // media is an image ?>
+						<img class="pages-media" src="<?php echo $media['media_url']; ?>" />
+					<?php else : // media is a video ?>
+						<video class="pages-media" controls>
+							<source src="<?php echo $media['media_url']; ?>" >
+						</video>
+					<?php endif; ?>
+					<h4>
+						<?php echo nl2br( $media['caption'] ); // display the caption preserving spaces ?>
+					</h4>
+					<div>
+						Link to Post:
+						<br />
+						<a target="_blank" href="<?php echo $media['permalink']; ?>">
+							<?php echo $media['permalink']; // link to media on instagram ?>
+						</a>
+					</div>
+					<br />
+					<div>
+						Post at: <?php echo $media['timestamp']; // time the media was posted ?>
+					</div>
+				</li>
+			<?php endforeach; ?>
+		</ul>
 
 
-<?php
-foreach ($instPosts as $v  )  {
-    echo $v['id'] . '<br>';
-}
-?>
-
-<?php /* only data, not 'paging'
-foreach ($instPosts['data'] as $v  )  {
-    echo $v['id'] . '<br>';
-}
-*/ ?>
 
 </body>
 </html>
